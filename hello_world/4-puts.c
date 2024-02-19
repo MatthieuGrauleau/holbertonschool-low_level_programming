3bc7f13a32c85403ed8dfb5ae3f28af0a8bb8d21
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-19 09:35:27 +0000

    using puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /*
- * main - main funcion
+ * main - main function
  * 
  * Return: Always returns 0 for success
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
+
