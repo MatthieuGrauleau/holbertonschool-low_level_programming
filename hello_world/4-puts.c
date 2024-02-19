487daa83bee6aaf85234da164365ca135ac5ad24
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-19 09:15:16 +0000

    using puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/*
+ * Fonction qui permet d'afficher un message sp√cifique et un retour a la ligne automatique gr√ce a puts
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
