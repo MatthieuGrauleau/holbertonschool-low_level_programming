0240a4983728574697b70ce8367524bb0f817f8f
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-19 10:09:59 +0000

    using printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - main function
+ *
+ * Description: this function prints a message using printf
+ *
+ * Return: Always return 0 for success.
+ */
+
 int main(void)
 {
-printf	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
