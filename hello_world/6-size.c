6cb3aeb6c2d87ed139db67a0ebaf6f151c0349e0
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-19 10:50:12 +0000

    prints the syze

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - function main
+ *
+ * Description:  prints the size of various types on the computer it is compiled and run on
+ *
+ * Return: Always return 0 for success.
+ */
+
+int main(void)
+{
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+}
