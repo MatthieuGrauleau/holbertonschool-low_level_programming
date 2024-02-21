2930ac73d5377b6125ceb65c68d8734d1ab29562
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-21 13:56:54 +0000

    test

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -13,13 +13,19 @@ void times_table(void)
 	int n;
 	int i;
 	int result;
-	
+	int a;
+	int b;
+
 	for (n = 0; n <= 9; n++)
 	{
 		for (i = 0; i <= 9; i++)
 		{
 			result = n * i;
-			 _putchar(result + '0');
+			a = result % 10;
+			b = (result - a) / 10;
+
+			_putchar(b + '0');
+			_putchar(a + '0');
 		}
 	}
 }	
