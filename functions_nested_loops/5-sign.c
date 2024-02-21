838e9ee4b7b9e1d3a33f41791d5b9f6ab67ff634
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-21 10:49:19 +0000

    rite a function that prints the sign of a number

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -18,11 +18,11 @@ int print_sign(int n)
 		return (1);
 	} else if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	} else
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 }
