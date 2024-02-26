fc7fea03e5e6284ceac48b1526028476a83433aa
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-26 12:28:04 +0000

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
@@ -28,11 +28,11 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		if ( i != 100)
+		if (i != 100)
 		{
 			printf(" ");
 		}
 	}
 	printf("\n");
-	return(0);
+	return (0);
 }
