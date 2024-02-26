ec38e6f0127d822dc85d54176751d3850393572e
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-26 13:00:58 +0000

    try

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - entry point
+ *
+ * Descrition: prints a triangle, followed by a new line
+ *
+ * Return: 0 for success.
+ */
+
+void print_triangle(int size)
+{
+	int count;
+	int space;
+
+	if (size > 0)
+	{
+		for (count = 0; count < size; count++)
+		{
+			for (space = 0; space - size; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+		}
+	}
+	_putchar('\n');
+}
