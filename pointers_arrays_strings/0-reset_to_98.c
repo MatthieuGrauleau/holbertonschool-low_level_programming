519898ca496679a6f3c02057dcaa1d1fcebea263
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-27 08:30:02 +0000

    creation of readme and main.h

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point
+ *
+ * @n: value
+ *
+ * @Description: takes a pointer
+ *
+ * Return: 0 for success.
+ */
+
+void reset_to_98(int *n)
+{
+	n = 98;
+	return(n);
+}
