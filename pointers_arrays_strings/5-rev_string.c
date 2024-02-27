e5326f608dbf546aae01646674b5882d02bb3745
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-27 12:53:48 +0000

    hot fix

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ *
+ * @s: string
+ *
+ * Description: reverses a string
+ *
+ * Return: 0 for success.
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int i = 0;
+
+	while (s[len])
+	{
+		len++;
+	}
+}
