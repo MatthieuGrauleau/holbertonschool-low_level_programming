1ea4790049401da560028737d9a6f3a87be18f7e
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-27 15:09:06 +0000

    creation of 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_array - prints n ellements of an array
+ *
+ * @a: integer
+ * @b: integer
+ *
+ * Return: 0 for success.
+ */
+
+void print_array(int *a, int n)
+{
+
