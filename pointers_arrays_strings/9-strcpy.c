6323e53db02df257eccf1400317df55f2d0cfe26
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-28 09:53:59 +0000

    hot fix

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,6 +12,7 @@
 char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
+	char *finaldest = dest;
 
 	while (src[index])
 	{
@@ -19,6 +20,6 @@ char *_strcpy(char *dest, char *src)
 		index++;
 	}
 
-	dest = '\0';
-	return (dest);
+	dest += '\0';
+	return (finaldest);
 }
