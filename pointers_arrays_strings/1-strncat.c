f3d31075ea185b1a763a8f278230ad4652ea29b8
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-28 13:20:45 +0000

    test

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates twho strings
+ *
+ * @dest: destination
+ * @src: source
+ * @n: nombre de caract√res
+ *
+ * Return: the resulting string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0;
+	int destlen = 0;
+
+	while (dest[index++])
+	{
+		destlen++;
+	}
+	for (index = 0; src[index] <= src[n - 1]; index++)
+	{
+		dest[destlen++] = src[index];
+	}
+	return (dest);
+}
