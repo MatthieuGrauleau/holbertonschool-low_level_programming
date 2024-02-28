db0bf77f7caa49144516e1ecd046beaed199eb8b
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-28 13:38:48 +0000

    test

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,12 +14,26 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0;
 	int destlen = 0;
+	int indexsrc = 0;
+	int totallen = 0;
 
 	while (dest[index++])
 	{
 		destlen++;
 	}
-	for (index = 0; src[index] <= src[n + 2] && src[index] != '\0'; index++)
+	while (src[indexsrc++])
+	{
+		indexsrc++;
+	}
+
+	totallen = destlen + indexsrc;
+	if (n < totallen)
+	{
+		for (index = 0; src[index] <= src[n - 1] && src[index] != '\0'; index++)
+		{
+			dest[destlen++] = src[index];
+		}
+	} else 
 	{
 		dest[destlen++] = src[index];
 	}
