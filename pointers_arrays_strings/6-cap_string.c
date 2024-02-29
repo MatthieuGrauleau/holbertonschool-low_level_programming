c038d4672a0c192735484b7521b91eca339cac53
MatthieuGrauleau <m.grauleau@gmail.com>
2024-02-29 16:15:28 +0000

    final

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize all word of a string
+ * cap_string - capitalize all word of a string.
  *
  * @a: string
  *Return: 0 for success.
@@ -13,6 +13,8 @@ char *cap_string(char *a)
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
+		if (i == 0 && a[i] >= 97 && a[i] <= 122)
+			a[i++] -= 32;
 		if (a[i] == ' ' ||
 			a[i] == '\t' ||
 			a[i] == '\n' ||
@@ -27,11 +29,9 @@ char *cap_string(char *a)
 			a[i] == '{' ||
 			a[i] == '}')
 		{
-			i++;
-
-			if (a[i] >= 97 && a[i] <= 122)
+			if (a[i + 1] >= 97 && a[i + 1] <= 122)
 			{
-				a[i] -= 32;
+				a[i + 1] -= 32;
 			}
 		}
 	}
