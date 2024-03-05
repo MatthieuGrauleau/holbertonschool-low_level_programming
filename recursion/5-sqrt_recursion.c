9b85a9c286103c9a2e18bedbccc2dbd0598a5d77
MatthieuGrauleau <m.grauleau@gmail.com>
2024-03-05 15:58:03 +0000

    creation of the file

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a num
+ *
+ * @n: integrer
+ * Return: the natural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+	int result;
