e391e25eb43f02a6fb3737d4a34c93211d17f210
MatthieuGrauleau <m.grauleau@gmail.com>
2024-03-11 09:27:39 +0000

    function that creates an array of chars, and initializes it with a specific char.

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars, and initializes it
+ *  with a specific char
+ *
+ *  @size: size of
+ *  @c: character
+ *
+ *  Return: NULL if size = 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(char) * size);
+
+	for (i = 0; i <= size; i++)
+	{
+		array[i] = c;
+	}
+	return (array);
+
+}
