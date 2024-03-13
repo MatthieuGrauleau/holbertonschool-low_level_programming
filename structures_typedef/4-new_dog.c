edebc175a96a03bdf82a47ae4bb7233abc0adcf4
MatthieuGrauleau <m.grauleau@gmail.com>
2024-03-13 10:25:48 +0000

    Write a function that creates a new dog.
    Prototype: dog_t *new_dog(char *name, float age, char *owner);
    You have to store a copy of name and owner
    Return NULL if the function fails

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * new_dog - function that creates a new dog
+ *
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Return: NULL if the function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
