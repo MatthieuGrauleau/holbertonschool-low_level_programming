1a3155233645f0e7428b37c2b6add40b59ba14ca
MatthieuGrauleau <m.grauleau@gmail.com>
2024-03-18 10:02:17 +0000

    body guard the h

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a function given as a paremeter on 
+ * each element of an array.
+ *
+ * @array: array
+ * @size: size of the array
+ * @action: a pointer to the function to be executed
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef FUNCTION_POINTERS_H_
+#define FUNCTION_POINTERS_H_
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,3 +7,5 @@ void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
+
+#endif
