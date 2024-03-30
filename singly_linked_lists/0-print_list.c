2ed1655c57be3bd5b55115d1ba56a3f7cdb59c2b
MatthieuGrauleau <m.grauleau@gmail.com>
2024-03-30 18:38:44 +0000

    creation of our size_t print_list(const list_t *h) function

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t
+ *
+ * @list_t: list
+ *
+ * Return: The number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+
