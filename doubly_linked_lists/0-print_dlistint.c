34cc0404c253f8b14cedb4f861278b47e6508acd
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-01 10:47:09 +0000

    adding return description

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,6 +4,8 @@
  * print_dlistint - function that prints al the elements of a dlistin_t list.
  *
  * @h: header of the list.
+ *
+ * Return: number of nodes.
  */
 
 size_t print_dlistint(const dlistint_t *h)
