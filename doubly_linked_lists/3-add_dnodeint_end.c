fe99929f21f6338d8804da5f1d997a3aa2a38818
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-02 10:23:00 +0000

    adding a space

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -29,7 +29,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	while (last_node->next != NULL)
 	{
-		last_node =last_node->next;
+		last_node = last_node->next;
 	}
 
 	last_node->next = new_node;
