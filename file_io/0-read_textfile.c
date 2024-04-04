1f2a67bdc80be99ff31fd3c8278566f08690a22c
MatthieuGrauleau <m.grauleau@gmail.com>
2024-04-04 15:47:02 +0200

    set up files

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file and prints.
+ * 
+ * @filename: pointer to the name of the file
+ * @letters: nulber of letters.
+ * 
+ * Return: if function fails or filename is NULL.
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	
+}
